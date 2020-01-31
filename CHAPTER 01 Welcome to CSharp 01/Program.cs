3a74fe99526ac4faddfa2df6431051680af5b6d2
Ronak Sankhala <RonakSankhala@outlook.com>
2020-01-31 14:04:14 +0530

    Add project files.

---
+++ CHAPTER 01 Welcome to CSharp 01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHAPTER_01_Welcome_to_CSharp_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Ronak Sankhala");
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine(DateTime.DaysInMonth(2020,1));
+            Console.Read();
+
+        }
+    }
+}
---
+++ CHAPTER 01 Welcome to CSharp 01/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CHAPTER 01 Welcome to CSharp 01")]
+[assembly: AssemblyDescription("This Project is Developed by Ronak Sankhala")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TechSode IT Solutions")]
+[assembly: AssemblyProduct("CHAPTER 01 Welcome to CSharp 01")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("Author","Ronak Sankhala")]
+[assembly: AssemblyMetadata("Written Date","30 Jan 2020")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("66ca0d6b-6179-4ffb-a226-184c6f189ecc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
